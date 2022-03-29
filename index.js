06e7b29069fb9963d2c0436ac98b8c642c839c82
Prasundas99 <prasundas115@gmail.com>
2022-03-29 19:23:13 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,46 @@
+//@ts-check
+
+export const configFunction = () => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  return config;
+};
+
+/**
+ * @param {{token: string}} userInfo
+ * @returns a config cunction which requires a bearer token with it
+ */
+export const tokenconfigFunction = (userInfo) => {
+  if (userInfo.token == undefined) {
+    return undefined;
+  }
+  const { token } = userInfo;
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  return config;
+};
+
+
+/**
+ * @param {Object} getState
+ * @returns a config cunction which requires a bearer token with it in redux
+ */
+export const reduxConfigFunction = (getState) => {
+  const {
+    userLogin: { data },
+  } = getState();
+  const config = {
+    "Content-Type": "application/json",
+    headers: {
+      Authorization: `Bearer ${data.token}`,
+    },
+  };
+  return config;
+};
